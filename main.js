38dba2210d82e1b1ebd7ac3ae095532fab349c10
tasnimzotder <tasnimzotder@gmail.com>
2019-12-06 20:30:01 +0530

    init deploy

---
+++ main.js
@@ -0,0 +1,35 @@
+// import { app, BrowserWindow } from "electron";
+const { app, BrowserWindow } = require("electron");
+require("electron-reload")(__dirname);
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 350,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
